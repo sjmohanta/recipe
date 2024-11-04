b5b58072d544086f68e7392002adc9cc9cddf3ab
Satyajit Mohanta <satyajitm@mindfiresolutions.com>
2024-11-04 18:31:07 +0530

    Added header with nav menus

--- recipe/src/RecipeApp.js
+++ recipe/src/RecipeApp.js
@@ -1,25 +1,34 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import logo from './logo.svg';
 import './App.css';
 
 export default function RecipeApp() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">          
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Recipe Book</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarText">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Recipies</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Add</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Pricing</a>
+                        </li>
+                    </ul>
+                    <span class="navbar-text">
+                        <a class="nav-link" href="#">Login</a>
+                    </span>
+                </div>
+            </div>
+        </nav>
     </div>
   );
 }
