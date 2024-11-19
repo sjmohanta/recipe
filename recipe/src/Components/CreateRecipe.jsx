cb50fd2c003f4f5e176b8146466e7f73345901f7
Satyajit Mohanta <satyajitm@mindfiresolutions.com>
2024-11-19 19:05:05 +0530

    Add Recipe : Added on change event handlers

--- recipe/src/Components/CreateRecipe.jsx
+++ recipe/src/Components/CreateRecipe.jsx
@@ -4,11 +4,11 @@ import TopNav from "./TopNav";
 export default function CreateRecipe()
 {
     const [recipe, updateRecipeState] = useState({
-        Name: undefined,
-        NoOfintegrands: 4,
-        Integrends: [],
-        NoOfSteps: 6,
-        Steps: []
+        name: undefined,
+        noOfintegrands: 4,
+        integrends: [],
+        noOfInstructions: 6,
+        instructions: []
     });
 
     function validateForm()
@@ -18,21 +18,21 @@ export default function CreateRecipe()
 
     function increaseNoOfIntegrands()
     {
-        updateRecipeState({...recipe, NoOfintegrands: ++recipe.NoOfintegrands});
+        updateRecipeState({...recipe, noOfintegrands: ++recipe.noOfintegrands});
     }
 
     function decreaseNoOfIntegrands()
     {
-        if (recipe.NoOfintegrands == 1)
+        if (recipe.noOfintegrands == 1)
         {
             return; // can not have zero integrands
         }
-        updateRecipeState({...recipe, NoOfintegrands: --recipe.NoOfintegrands});
+        updateRecipeState({...recipe, noOfintegrands: --recipe.noOfintegrands});
     }
 
     function increaseNoOfSteps()
     {
-        updateRecipeState({...recipe, NoOfSteps: ++recipe.NoOfSteps});
+        updateRecipeState({...recipe, noOfInstructions: ++recipe.noOfInstructions});
     }
 
     function decreaseNoOfSteps()
@@ -42,7 +42,24 @@ export default function CreateRecipe()
             return; // can not have zero steps
         }
 
-        updateRecipeState({...recipe, NoOfSteps: --recipe.NoOfSteps});
+        updateRecipeState({...recipe, noOfInstructions: --recipe.noOfInstructions});
+    }
+
+    function recipeNameChanged(e)
+    {
+        updateRecipeState({...recipe, name: e.target.value});
+    }
+
+    function integrandChanged(e, position)
+    {
+        recipe.integrends[position] = e.target.value;
+        updateRecipeState({...recipe, integrends: recipe.integrends});
+    }
+
+    function instructionChanged(e, position)
+    {
+        recipe.instructions[position] = e.target.value;
+        updateRecipeState({...recipe, instructions: recipe.instructions});
     }
 
     return <>
@@ -53,14 +70,14 @@ export default function CreateRecipe()
         <form className="col-8">
             <div class="mb-3">
                 <label for="txtRecipeName" class="form-label">Name</label>
-                <input class="form-control" id="txtRecipeName" placeholder="Recipe Name" required />
+                <input class="form-control" id="txtRecipeName" placeholder="Recipe Name" onChange={recipeNameChanged} required />
             </div>            
             <div class="mb-3">
                 <label class="form-label">Integrends</label>
                 <span class="btn btn-outline-secondary btn-sm ms-3 me-1" onClick={increaseNoOfIntegrands}>+</span>
                 <span class="btn btn-outline-secondary btn-sm" onClick={decreaseNoOfIntegrands}>-</span>
                 {
-                    [...Array(recipe.NoOfintegrands).keys()].map(key => <input id={`txtIntegrend${key + 1}`} className="form-control mb-1" 
+                    [...Array(recipe.noOfintegrands).keys()].map(key => <input onChange={(e) => integrandChanged(e, key)} id={`txtIntegrend${key + 1}`} className="form-control mb-1" 
                         placeholder={`Integrend ${key + 1}`} required />)
                 }
             </div>
@@ -69,8 +86,8 @@ export default function CreateRecipe()
                 <span class="btn btn-outline-secondary btn-sm ms-3 me-1" onClick={increaseNoOfSteps}>+</span>
                 <span class="btn btn-outline-secondary btn-sm" onClick={decreaseNoOfSteps}>-</span>
                 {
-                    [...Array(recipe.NoOfSteps).keys()].map(key => <input id={`txtStep${key + 1}`} className="form-control mb-1" 
-                        placeholder={`Step ${key + 1}`} required />)
+                    [...Array(recipe.noOfInstructions).keys()].map(key => <input onChange={(e) => instructionChanged(e, key)} id={`txtStep${key + 1}`} className="form-control mb-1" 
+                        placeholder={`Instruction ${key + 1}`} required />)
                 }
             </div>
             <button type="button" class="btn btn-primary" onClick={validateForm}>Create</button>
