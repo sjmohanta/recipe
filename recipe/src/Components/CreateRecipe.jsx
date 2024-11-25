2720866c48ffb422ff001c7f304b934e420a4684
Satyajit Mohanta <satyajitm@mindfiresolutions.com>
2024-11-25 22:37:34 +0530

    Create recipe : added form validation

--- recipe/src/Components/CreateRecipe.jsx
+++ recipe/src/Components/CreateRecipe.jsx
@@ -19,6 +19,14 @@ export default function CreateRecipe()
         image: undefined
     });
 
+    const [recipeValidation, updateRecipeValidationState] = useState({
+        isNameEmpty: undefined,
+        isPrepTimeValid: undefined,
+        isAnyIntegrandsInputed: undefined,
+        isAnyInstructionsInputed: undefined,
+        isImageSelected: undefined
+    });
+
     var authInfo = getAuthInfo();
     if (!authInfo)
     {
@@ -36,7 +44,12 @@ export default function CreateRecipe()
 
     function validateForm()
     {
-        // To-do : Validate
+        if (!(recipeValidation.isNameEmpty === false && recipeValidation.isPrepTimeValid && recipeValidation.isAnyIntegrandsInputed 
+            && recipeValidation.isAnyInstructionsInputed && recipeValidation.isImageSelected))
+        {
+            return;
+        }
+
         const apiRootUrl = appConfig("ApiRootPath");
         const {noOfintegrands, noOfInstructions, ...dataToPost} = {...recipe, userId: authInfo.uid};
 
@@ -48,8 +61,7 @@ export default function CreateRecipe()
             headers: {'Content-Type' : 'application/json'},
             body: JSON.stringify({...dataToPost})
         })
-        .then(function (response) {      
-            
+        .then(function (response) { 
             if(response.ok){
                 response.json().then(function (result) {                    
                     debugger;
@@ -94,43 +106,63 @@ export default function CreateRecipe()
 
     function recipeNameChanged(e)
     {
-        updateRecipeState({...recipe, name: e.target.value});
+        var recipeName = e.target.value;
+        recipeValidation.isNameEmpty = recipeName.length === 0;
+        updateRecipeValidationState({...recipeValidation});
+        updateRecipeState({...recipe, name: recipeName});
     }
 
     function integrandChanged(e, position)
     {
         recipe.integrands[position] = e.target.value;
         updateRecipeState({...recipe, integrands: recipe.integrands});
+        
+        recipeValidation.isAnyIntegrandsInputed = recipe.integrands.findIndex(function (integrand) {
+            return integrand.length > 0;
+        }) !== -1;
+        updateRecipeValidationState({...recipeValidation});
     }
 
     function instructionChanged(e, position)
     {
         recipe.instructions[position] = e.target.value;
         updateRecipeState({...recipe, instructions: recipe.instructions});
+
+        recipeValidation.isAnyInstructionsInputed = recipe.instructions.findIndex(function (instruction) {
+            return instruction.length > 0;
+        }) !== -1;
+        updateRecipeValidationState({...recipeValidation});
     }    
 
     function recipeImageChanged(e)
     {
-        if (e.target.files)
+        if (e.target.files.length)
         {
             // convert image to base 64
-            getBase64(e.target.files[0], function (base64Img) {
-                debugger;
+            getBase64(e.target.files[0], function (base64Img) {                
                 updateRecipeState({...recipe, image: base64Img});
             }, 
             function (e) {
                 console.warn(e);
             });
+
+            recipeValidation.isImageSelected = true;
         }
         else{
             updateRecipeState({...recipe, image: undefined});
-        }        
+            recipeValidation.isImageSelected = false;
+        }     
+        
+        updateRecipeValidationState({...recipeValidation});
     }
 
     function recipePreparationTimeChanged(e)
     {
         recipe.prepTimeMinutes = e.target.value;
         updateRecipeState({...recipe});
+
+        recipeValidation.isPrepTimeValid = recipe.prepTimeMinutes.length > 0;
+        updateRecipeValidationState({...recipeValidation});
     }
 
     return <>
@@ -143,14 +175,23 @@ export default function CreateRecipe()
                 <div className="mb-3">
                     <label htmlFor="txtRecipeName" className="form-label">Name <span className="text-danger">*</span></label>
                     <input className="form-control" id="txtRecipeName" placeholder="Recipe Name" onChange={recipeNameChanged} required />
+                    {recipeValidation.isNameEmpty && <div class="invalid-feedback">
+                            Please provide recipe name.
+                        </div>}
                 </div>  
                 <div className="mb-3">
                     <label htmlFor="txtPreparationTime" className="form-label">Preparation time (minutes) <span className="text-danger">*</span></label>
-                    <input className="form-control" id="txtPreparationTime" placeholder="Preparation time (minutes)" onChange={recipePreparationTimeChanged} required />
+                    <input className="form-control" type="number" id="txtPreparationTime" placeholder="Preparation time (minutes)" onChange={recipePreparationTimeChanged} required />
+                    {(recipeValidation.isPrepTimeValid === false) && <div class="invalid-feedback">
+                            Please provide recipe preparation time.
+                        </div>}
                 </div>
                 <div className="mb-3">
                     <label htmlFor="recipeImage" className="form-label">Image <span className="text-danger">*</span></label>
                     <input type="file" accept="image/*" className="form-control" id="recipeImage" onChange={recipeImageChanged} required />
+                    {(recipeValidation.isImageSelected === false) && <div class="invalid-feedback">
+                            Please select image for recipe.
+                        </div>}
                 </div>          
                 <div className="mb-3">
                     <label className="form-label">Integrends <span className="text-danger">*</span></label>
@@ -160,6 +201,9 @@ export default function CreateRecipe()
                         [...Array(recipe.noOfintegrands).keys()].map(key => <input onChange={(e) => integrandChanged(e, key)} key={`txtIntegrend${key + 1}`} className="form-control mb-1" 
                             placeholder={`Integrend ${key + 1}`} required />)
                     }
+                    {(recipeValidation.isAnyIntegrandsInputed === false) && <div class="invalid-feedback">
+                            Please add some integrands.
+                        </div>}
                 </div>
                 <div className="mb-3">
                     <label className="form-label" required>Instructions <span className="text-danger">*</span></label>
@@ -169,6 +213,9 @@ export default function CreateRecipe()
                         [...Array(recipe.noOfInstructions).keys()].map(key => <input onChange={(e) => instructionChanged(e, key)} key={`txtStep${key + 1}`} className="form-control mb-1" 
                             placeholder={`Instruction ${key + 1}`} required />)
                     }
+                    {(recipeValidation.isAnyInstructionsInputed === false) && <div class="invalid-feedback">
+                            Please add some instructions to prepare the recipe.
+                        </div>}
                 </div>
                 <button type="button" className="btn btn-primary" onClick={validateForm}>Create</button>
             </form>
