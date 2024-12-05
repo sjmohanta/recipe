1866f258f17dd58e2c032dace8a523ba03325b5d
Satyajit Mohanta <satyajitm@mindfiresolutions.com>
2024-12-05 22:42:41 +0530

    Auth Api - return name of user to use in header

--- authapi/routes/auth.js
+++ authapi/routes/auth.js
@@ -34,9 +34,8 @@ router.post('/signup', async (req, res, next) => {
   try {
     const createdUser = await add(data);
     const authToken = createJSONToken(createdUser.email);
-    res
-      .status(201)
-      .json({ message: 'User created.', user: createdUser, token: authToken });
+    res.status(201)
+      .json({ uid: createdUser.id, name: data.name, token: authToken });
   } catch (error) {
     next(error);
   }
