53e6b18d8c8616d11b22bcc69343bb2f532a237e
Satyajit Mohanta <satyajitm@mindfiresolutions.com>
2024-12-11 21:19:17 +0530

    Recipe details : Reorganized components to use recipe details info to add new review

--- recipe/src/Components/Recipe/RecipeDetails.jsx
+++ recipe/src/Components/Recipe/RecipeDetails.jsx
@@ -13,44 +13,65 @@ export default function RecipeDetails()
         document.title = `${name} Recipe`;
 
         return <>
-            <h2 className="text-info">{name}</h2>
-            <span>
-                <i className="fa fa-clock"></i> Preparation time: 
-                 <Link className="ms-2" to={`/Recipes/PreparationTime?preparationTime=${prepTimeMinutes}`}>{prepTimeMinutes} minutes</Link>
-            </span>
-            <hr/>
-            <div className="col-12 col-md-4">
-                <img src={image} className="img-thumbnail" alt="Recipe photo" />
-            </div>            
-            <h4 className="text-info">
-                Integrends
-            </h4>
-            <ul>
-                {integrands.map((ingredient) => {
-                    return <li key={ingredient}>
-                        <Link to={`/Recipes/Integrand?integrand=${ingredient}`}>
-                            {ingredient}
-                        </Link>                        
-                    </li>
-                })}
-            </ul>
-            <h4 className="text-info">
-                Instructions
-            </h4>
-            <ol>
-                {instructions.map((instruction) => {
-                    return <li key={instruction}>{instruction}</li>
-                })}
-            </ol>
+            <div className="row">
+                <div className="col-12">
+                    <h2 className="text-info">{name}</h2>
+                    <span>
+                        <i className="fa fa-clock"></i> Preparation time: 
+                        <Link className="ms-2" to={`/Recipes/PreparationTime?preparationTime=${prepTimeMinutes}`}>{prepTimeMinutes} minutes</Link>
+                    </span>
+                    <hr/>
+                    <div className="col-12 col-md-4">
+                        <img src={image} className="img-thumbnail" alt="Recipe photo" />
+                    </div>            
+                    <h4 className="text-info">
+                        Integrends
+                    </h4>
+                    <ul>
+                        {integrands.map((ingredient) => {
+                            return <li key={ingredient}>
+                                <Link to={`/Recipes/Integrand?integrand=${ingredient}`}>
+                                    {ingredient}
+                                </Link>                        
+                            </li>
+                        })}
+                    </ul>
+                    <h4 className="text-info">
+                        Instructions
+                    </h4>
+                    <ol>
+                        {instructions.map((instruction) => {
+                            return <li key={instruction}>{instruction}</li>
+                        })}
+                    </ol>
 
-            <h2 className="text-info">
-                Reviews
-            </h2>
-            <div>
-                <ShowRating rating={rating}></ShowRating>
-                <span className="ms-3"><Link to={`/Recipes/Rating?rating=${rating}`}>{rating?.toFixed(1)}</Link> based on {reviewCount} reviews.</span>                
+                    <h2 className="text-info">
+                        Reviews
+                    </h2>
+                    <div>
+                        <ShowRating rating={rating}></ShowRating>
+                        <span className="ms-3"><Link to={`/Recipes/Rating?rating=${rating}`}>{rating?.toFixed(1)}</Link> based on {reviewCount} reviews.</span>                
+                    </div>
+                    <hr/>
+                </div>
+                <div className="col-12">                    
+                    <div className="row">
+                        <div className="col-12 col-md-6">
+                            <Suspense fallback={<p>Loading reviews...</p>}>
+                                <Await resolve={recipeReviews}>
+                                    {(reviews) => <ReviewList reviews={reviews} />}
+                                </Await>
+                            </Suspense>
+                        </div>
+                        <div className="col-12 col-md-6">
+                            <div className="border-start border-2 border-info-subtle p-2">
+                                <h4 className="text-secondary">Add your review</h4>
+                                <AddReview recipeId={id} aggregatedRating={rating} reviewCount={reviewCount}></AddReview>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <hr/>
         </>;
     }
 
@@ -63,24 +84,7 @@ export default function RecipeDetails()
                             {(recipeInfo) => <ShowRecipe {...recipeInfo} />}
                         </Await>
                     </Suspense>                    
-                </div>
-                <div className="col-12">                    
-                    <div className="row">
-                        <div className="col-12 col-md-6">
-                            <Suspense fallback={<p>Loading reviews...</p>}>
-                                <Await resolve={recipeReviews}>
-                                    {(reviews) => <ReviewList reviews={reviews} />}
-                                </Await>
-                            </Suspense>                            
-                        </div>
-                        <div className="col-12 col-md-6">
-                            <div className="border-start border-2 border-info-subtle p-2">
-                                <h4 className="text-secondary">Add your review</h4>
-                                {/* <AddReview recipeId={id} aggregatedRating={rating} reviewCount={reviewCount}></AddReview> */}
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                </div>                
             </div>
         </div>        
     </>
